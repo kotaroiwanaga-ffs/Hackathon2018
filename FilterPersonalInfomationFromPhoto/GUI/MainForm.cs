700fb18717c7798e8cb66a2b3a7d559776f6be1b
kotaroiwanaga-ffs <38637016+kotaroiwanaga-ffs@users.noreply.github.com>
2019-02-28 16:17:44 +0900

    GUIの画像アスペクト比を補正

--- FilterPersonalInfomationFromPhoto/GUI/MainForm.cs
+++ FilterPersonalInfomationFromPhoto/GUI/MainForm.cs
@@ -6,34 +6,45 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using FilterPersonalInfomation;
+using System.IO;
 
 namespace GUI
 {
     public partial class MainForm : Form
     {
+        private OpenFileDialog ofd;
+
         public MainForm()
         {
             InitializeComponent();
-        }
+            this.ofd = new OpenFileDialog();
 
-        private void fileDialogue_button_Click(object sender, EventArgs e)
-        {
-            OpenFileDialog ofd = new OpenFileDialog();
-
-            ofd.InitialDirectory = @"C:\";
+            ofd.InitialDirectory = @"C:\OpenPose\sample\sample";
             ofd.Filter = "画像ファイル(*.png,*.jpg,*.bmp,*.gif)|*.png;*.jpg;*.bmp;*.gif|すべてのファイル(*.*)|*.*";
             ofd.FilterIndex = 1;
             ofd.RestoreDirectory = true;
+        }
 
+
+        private void fileDialogue_button_Click(object sender, EventArgs e)
+        {
+            
 
             if (ofd.ShowDialog() == DialogResult.OK)
             {
                 filePath_textBox.Text = ofd.FileName;
+                ofd.InitialDirectory = Path.GetDirectoryName(ofd.FileName);
+
                 try
                 {
                     selectedImage_pictureBox.ImageLocation = ofd.FileName;
                     Image image = Image.FromFile(ofd.FileName);
                     filtering_button.Enabled = true;
+
+                    selectedImage_pictureBox.Width = (int)(image.Width * ((double)selectedImage_pictureBox.Height / image.Height));
+                    selectedImage_pictureBox.Left = (int)((this.Width - selectedImage_pictureBox.Width) / 2);
+
+                    //selectedImage_pictureBox.Width = 300;
 
                 }
                 catch (Exception)
